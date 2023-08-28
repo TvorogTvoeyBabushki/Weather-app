2b387440510600d3a68f5c264a070efda274245f
foxta1ll <snake3773@mail.ru>
2023-08-28 12:09:28 +0300

    init: инициализация проекта

---
+++ src/app.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import Home from './components/screens/home/Home'
+import '@/assets/styles/global.scss'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+	<React.StrictMode>
+		<Home />
+	</React.StrictMode>
+)
---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import { resolve } from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [react(), tsconfigPaths()],
+	resolve: {
+		alias: {
+			'@': resolve(__dirname, './src')
+		},
+		extensions: ['.ts', '.tsx', 'json']
+	},
+	server: {
+		port: 7000,
+		open: true,
+		cors: true
+	},
+	base: './'
+})
